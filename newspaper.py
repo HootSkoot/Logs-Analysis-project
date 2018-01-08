5598da3df3d358bdb1ad8f035cd27ac2ea729853
HootSkoot <Michael.Nishimura08@gmail.com>
2018-01-08 01:56:21 -0800

    Add files via upload

--- newspaper.py
+++ newspaper.py
@@ -1,4 +1,5 @@
 import psycopg2
+#!/usr/bin/env python
 
 
 def printRows(list, viewType):
@@ -15,29 +16,29 @@ db = psycopg2.connect(database='news')
 cursor = db.cursor()
 """this view creates a table with the article titles, authors,
 and view counts used for the next two steps"""
-cursor.execute("create view article_views as select articles.title, \
-               articles.author, count(articles.slug) as views\
-               from articles join log on log.path \
-               like concat('%', articles.slug) \
-               group by articles.title, articles.author \
-               order by views desc;")
-cursor.execute('select title, views from article_views limit 3;')
+cursor.execute("CREATE OR REPLACE VIEW article_views AS SELECT articles.title, \
+               articles.author, COUNT(articles.slug) AS views\
+               FROM articles JOIN log ON log.path \
+               LIKE CONCAT('%', articles.slug) \
+               GROUP BY articles.title, articles.author \
+               ORDER BY views DESC;")
+cursor.execute('SELECT title, views FROM article_views LIMIT 3;')
 print "The top 3 most viewed articles:\n"
 printRows(cursor.fetchall(), "views")
-cursor.execute('select authors.name, sum(article_views.views) as views \
-               from article_views \
-               join authors on article_views.author = authors.id \
-               group by authors.name \
-               order by views desc')
+cursor.execute('SELECT authors.name, SUM(article_views.views) AS views \
+               FROM article_views \
+               JOIN authors ON article_views.author = authors.id \
+               GROUP BY authors.name \
+               ORDER BY views DESC')
 print "\nHere are the authors ranked in terms of greatest number of viewers:"
 printRows(cursor.fetchall(), "views")
-cursor.execute("select time::date as date, \
-               round( count ( case when status like '%404%' then 1 end ) \
-               * 100::numeric / count(*) , 2 ) as percent_fail \
-               from log \
-               group by date \
-               having (count(case when status like '%404%' then 1 end) \
-               * 100)::numeric / count(*) > 1.0;")
+cursor.execute("SELECT time::date AS date, \
+               ROUND( COUNT ( CASE WHEN status LIKE '%404%' THEN 1 END ) \
+               * 100::numeric / COUNT(*) , 2 ) AS percent_fail \
+               FROM log \
+               GROUP BY date \
+               HAVING (COUNT(CASE WHEN status LIKE '%404%' THEN 1 END) \
+               * 100)::numeric / COUNT(*) > 1.0;")
 print "\nThese are the dates where >1% of users could not load an article:"
 printRows(cursor.fetchall(), "precentage failure")
 db.close()
